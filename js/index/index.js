7cc7b9e9c398ea068fe42e335e35f75e7b831b08
fx35792 <1873005958@qq.com>
2016-04-02 18:17:06 +0800

    add file

---
+++ js/index/index.js
@@ -0,0 +1,16 @@
+$(function(){
+	//幻灯片自动播放
+	$("#myCarousel").carousel({
+		//4秒后自动播放
+		interval:4000,
+	});
+	//幻灯片垂直居中
+	// $(".carousel-control").css("line-height",$(".carousel-inner img").height() + "px");
+	// $(window).resize(function() {
+	// 	//不管是哪张幻灯片都要做到垂直居中
+	// 	var $hdpImg = $(".carousel-inner img").eq(0).height() ||
+	// 				  $(".carousel-inner img").eq(1).height() ||
+	// 				  $(".carousel-inner img").eq(2).height(); 
+	// 	$(".carousel-control").css("line-height",$hdpImg + "px");
+	// });
+});

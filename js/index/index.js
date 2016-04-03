f64fde18674fc26ad6252ffa072ff3f5478fdf58
fx35792 <1873005958@qq.com>
2016-04-03 18:31:48 +0800

    add file

--- js/index/index.js
+++ js/index/index.js
@@ -13,4 +13,17 @@ $(function(){
 	// 				  $(".carousel-inner img").eq(2).height(); 
 	// 	$(".carousel-control").css("line-height",$hdpImg + "px");
 	// });
+
+	//左右图片文字垂直居中
+	
+	$(".text").eq(0).css("margin-top",($(".auto").eq(0).height()-$(".text").eq(0).height())/2+"px");
+	$(window).resize(function() {
+		$(".text").eq(0).css("margin-top",($(".auto").eq(0).height()-$(".text").eq(0).height())/2+"px");
+	});
+
+	$(".text").eq(1).css("margin-top",($(".auto").eq(1).height()-$(".text").eq(1).height())/2+"px");
+	$(window).resize(function() {
+		$(".text").eq(1).css("margin-top",($(".auto").eq(1).height()-$(".text").eq(1).height())/2+"px");
+	});
+
 });
